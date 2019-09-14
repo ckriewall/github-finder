90b2f8eb8202a07c15f9157ee6682e89da717488
Charlie Kriewall <charlie@kriewall.com>
2019-09-13 17:26:29 -0700

    add GitHub api call and Spinner component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data, loading: false });
+    console.log(res.data);
+  }
+
   render() {
     return (
       <div>
@@ -11,7 +26,7 @@ class App extends Component {
           <Navbar title='Github Finder' icon='fab fa-github' />
         </nav>
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,47 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
-class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'ckriewall',
-        avatar_url:
-          'https://avatars1.githubusercontent.com/u/16513447?s=40&v=4',
-        html_url: 'https://github.com/ckriewall'
-      },
-      {
-        id: '2',
-        login: 'ckriewall',
-        avatar_url:
-          'https://avatars1.githubusercontent.com/u/16513447?s=40&v=4',
-        html_url: 'https://github.com/ckriewall'
-      },
-      {
-        id: '3',
-        login: 'ckriewall',
-        avatar_url:
-          'https://avatars1.githubusercontent.com/u/16513447?s=40&v=4',
-        html_url: 'https://github.com/ckriewall'
-      }
-    ]
-  };
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => (
+        {users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+};
 
 const userStyle = {
   display: 'grid',
   gridTemplateColumns: 'repeat(3, 1fr)',
   gridGap: '1rem'
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
 };
 
 export default Users;
